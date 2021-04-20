4584a5960cfd47121f80f5ea8749f75304124b80
mojopi <molly.josephs@gmail.com>
2021-04-20 14:39:01 -0400

    adding presentation for hgse

---
+++ pbl/imgs/cutouts.js
@@ -0,0 +1,104 @@
+ //on attempt at making the card disappear and description show//
+
+
+  // description.style.width = "100%";
+                    // description.style.height = "100%";
+                    // description.style.zIndex = "-10";
+                    // description.style.position = "relative";
+
+
+
+
+                    // description.style.borderRadius = "10%";
+                    // description.style.top = "0%";
+                    // description.style.left = "0%";
+
+                    // function timer() {
+                    //     console.log(time)
+                    //     time = time + 1;
+                    //     if (time < 2) { headshot.id = "no-id" } else if (time > 2) {
+                    //         headshot.id = "headshot" + i;
+                    //         clearInterval();
+                    //     }
+
+                    // };
+
+                    // headshot.addEventListener(
+                    //     "click",
+                    //     function() { setInterval(timer, 1000) });
+
+
+                    // headshot.addEventListener(
+                    //     "click",
+                    //     function() {
+                    //     	console.log("hi!");
+                    //         // headshot.classList.toggle("no-headshot") ;
+                    //     })
+
+                    // headshot.style.transition = "1s";
+                    // setInterval(function time() {timer = timer + 1 }, 1000)
+
+                    // if (timer=7) {
+                    // 	headshot.style.display="show";
+
+
+
+                    // headshot.style.width = "15vw";
+                    // headshot.style.height = "15vw";
+                    // headshot.style.zIndex = "10";
+                    // headshot.style.position = "relative";
+                    // headshot.style.display = "inline-block";
+                    // headshot.style.backgroundSize = "cover";
+                    // headshot.style.backgroundPosition = "center";
+                    // headshot.style.borderRadius = "10%";
+                    // headshot.style.top = "0%";
+                    // headshot.style.left = "0%";
+
+
+                    // headshot.style.marginRight = "2%";
+                    // headshot.style.marginBottom = "5%";
+
+                    // // headshot.style.backgroundColor = "green";
+                    // headshot.style.border = "1px grey solid";
+
+                                        // description.style.display = "inline-block";
+
+
+
+// function makeName()  {
+
+                //     var self = this;
+                //     self.name = document.createElement("div");
+                //     // document.getElementById("headshot" + i).appendChild(self.name);
+
+                //     document.body.appendChild(self.name);
+                //     self.name.classList.add("name");
+                //     self.name.id = "name" + i;
+                //     self.name.style.height = "4vw";
+                //     self.name.style.textAlign = "center";
+                //     // name.style.backgroundColor="red";
+                //     self.name.style.position = "relative";
+                //     self.name.style.top = "112%";
+                //     self.name.style.fontSize = "3.0vw";
+                //     self.name.style.fontFamily = "'EB Garamond', serif";
+                //     console.log(name.id);
+
+                //     self.name.style.color = "rgb(105, 105, 105)";
+
+                //     self.name.addEventListener("mouseenter", function() {
+                //         name.style.color = "blue";
+
+                //     })
+
+                //     self.name.addEventListener("mouseleave", function() {
+                //         name.style.color = "grey";
+
+                //     })
+                // }
+
+                // makeName();
+
+
+               
+               
+               
